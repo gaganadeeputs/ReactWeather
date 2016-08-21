3afc7cba173d76f38a74ef30a72d55944d29284c
T S <gagandeepts@gmail.com>
2016-08-21 16:48:44 +0530

    major changese

--- app/api/OpenWeatherMap.jsx
+++ app/api/OpenWeatherMap.jsx
@@ -1,12 +1,13 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=4a7bd54d8b3f2d27561f595d853fbb02c752517b2d&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=4a7bd54d8b3f2d27561f595d853fbb02&units=imperial';
 
 module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
+   debugger;
     return axios.get(requestUrl).then(function (res) {
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
