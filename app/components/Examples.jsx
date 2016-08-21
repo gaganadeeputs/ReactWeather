2a0e83fd15b610b67126ee303735812cae603046
T S <gagandeepts@gmail.com>
2016-08-21 13:35:02 +0530

    Rename example Page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,9 +4,7 @@ var Examples= React.createClass({
 render: function() {
    return(
      <div>
-           <h3>Example component!!!</h3>
-           <h3>Welcome to Examples!!!</h3>
-           <h4>Welcome to Examples!!!</h4>
+           <h3>Examples!!!</h3>
      </div>
 
    );
